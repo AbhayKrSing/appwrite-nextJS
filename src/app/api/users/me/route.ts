7bcbf5fdd4a2aaaf210896ed3011032d0b93a6cb
Abhay <kabhay849@gmail.com>
2023-09-26 00:55:14 +0530

    commiting to debug vecel deployment

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,17 +1,17 @@
-import connectdb from "@/dbConfig/dbConfig"
-import { getDataFromToken } from "@/helpers/getDataFromToken"
-import User from "@/models/User"
-import { NextRequest, NextResponse } from "next/server"
-connectdb()
-export const GET= async (req:NextRequest) => {
-    try {
-        const userData:any=  await getDataFromToken(req)
-         const user= await User.findById(userData.id).select('-password')
-         return NextResponse.json(user)
-     
-    } catch (error:any) {
-      return  NextResponse.json({
-            message:error.message
-        })
-    }
-}
+import connectdb from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+connectdb();
+export const GET = async (req: NextRequest) => {
+  try {
+    const userData: any = await getDataFromToken(req);
+    console.log(userData); //will remove after vercel debugged happens
+    const user = await User.findById(userData?.id).select("-password");
+    return NextResponse.json(user);
+  } catch (error: any) {
+    return NextResponse.json({
+      message: error.message,
+    });
+  }
+};
--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,13 +1,15 @@
 import jwt from "jsonwebtoken";
 import { NextRequest } from "next/server";
 
-export async function getDataFromToken(req:NextRequest) {
-    try {
-        const token=  req.cookies.get('token')?.value|| ""
-        //Verify token
-      const Userdata=  jwt.verify(token,process.env.TOKEN_SECRET!)
-      return Userdata
-    } catch (error:any) {
-        console.log(error.message)
-    }
+export async function getDataFromToken(req: NextRequest) {
+  try {
+    const token = req.cookies.get("token")?.value || "";
+    console.log(token); //will remove after vercel debugged happens
+    //Verify token
+    const Userdata = jwt.verify(token, process.env.TOKEN_SECRET!);
+    console.log(Userdata);
+    return Userdata;
+  } catch (error: any) {
+    console.log(error.message);
+  }
 }
